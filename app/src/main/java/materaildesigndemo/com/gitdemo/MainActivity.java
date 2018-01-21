bdb0cc8b9056f8a488069668e932b90771834193
zhengxudong1314 <a634540618@163.com>
2018-01-21 13:57:48 +0800

    Initial commit

---
+++ app/src/main/java/materaildesigndemo/com/gitdemo/MainActivity.java
@@ -0,0 +1,12 @@
+package materaildesigndemo.com.gitdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
